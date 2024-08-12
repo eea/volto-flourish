be9fde6bf2a8bcb9ab450d364c6f43b2ca2ad13f
Tripon Eugen <tripon.eugen@gmail.com>
2024-08-12 10:49:10 +0300

    iframe title

--- src/blocks/EmbedFlourish/View.js
+++ src/blocks/EmbedFlourish/View.js
@@ -3,7 +3,12 @@ const View = (props) => {
   return (
     <div className="embed-flourish">
       {props.data.flourish_item_url ? (
-        <iframe src={flourish_url} height="400" width="100%"></iframe>
+        <iframe
+          src={flourish_url}
+          height="400"
+          width="100%"
+          title="FlourishEmbed"
+        ></iframe>
       ) : props.mode ? (
         <div>Embed flourish</div>
       ) : null}
