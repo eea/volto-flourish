89caf4112ac53dd1a0ba904e32536d96fafc723c
Tripon Eugen <tripon.eugen@gmail.com>
2024-08-12 10:31:54 +0300

    display iframe only when url is set

--- src/blocks/EmbedFlourish/View.js
+++ src/blocks/EmbedFlourish/View.js
@@ -2,12 +2,9 @@ const View = (props) => {
   const flourish_url = props.data.flourish_item_url + '/@@flourish/index.html';
   return (
     <div className="embed-flourish">
-      <iframe
-        src={flourish_url}
-        height="400"
-        width="100%"
-        title="Iframe Example"
-      ></iframe>
+      {props.data.flourish_item_url ? (
+        <iframe src={flourish_url} height="400" width="100%"></iframe>
+      ) : null}
     </div>
   );
 };
