5cd1f8d472bd3f78a887b88a784e2859c7e752fd
Tripon Eugen <tripon.eugen@gmail.com>
2024-08-29 14:55:41 +0300

    add test

---
+++ src/blocks/EmbedFlourish/View.test.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
+import configureStore from 'redux-mock-store';
+import { render } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import { Provider } from 'react-intl-redux';
+import View from './View';
+
+const mockStore = configureStore();
+
+describe('View', () => {
+  it('should render the component', () => {
+    const store = mockStore({
+      userSession: { token: '1234' },
+      intl: {
+        locale: 'en',
+        messages: {},
+      },
+    });
+
+    const { container } = render(
+      <Provider store={store}>
+        <MemoryRouter>
+          <View />
+        </MemoryRouter>
+      </Provider>,
+    );
+    expect(container.querySelector('.embed-flourish')).toBeInTheDocument();
+  });
+});
