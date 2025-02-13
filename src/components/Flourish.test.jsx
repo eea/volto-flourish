bf01b2c280f699f620f1c8ee71b11921c9da7e55
kreafox <krisztina.n.elekes@gmail.com>
2025-02-13 12:18:02 +0200

    test: increase coverage

---
+++ src/components/Flourish.test.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import configureStore from 'redux-mock-store';
+import { Provider } from 'react-intl-redux';
+
+import Flourish from './Flourish';
+
+const mockStore = configureStore();
+
+jest.mock('./Flourish', () => () => <div className="flourish-embed" />);
+
+test('Flourish component', () => {
+  const store = mockStore({
+    intl: {
+      locale: 'en',
+      messages: {},
+    },
+  });
+  const baseUrl = 'http://example.com';
+
+  const component = renderer.create(
+    <Provider store={store}>
+      <Flourish baseUrl={baseUrl} />
+    </Provider>,
+  );
+  const json = component.toJSON();
+  expect(json).toMatchSnapshot();
+});
--- src/widgets/FlourishViewWidget.test.jsx
+++ src/widgets/FlourishViewWidget.test.jsx
@@ -13,7 +13,6 @@ const store = mockStore({
   },
 });
 
-// Mock the Flourish component
 jest.mock('@eeacms/volto-flourish/components/Flourish', () => () => (
   <div data-testid="flourish-component" />
 ));
@@ -23,16 +22,13 @@ test('renders FlourishViewWidget component', () => {
     download: 'http://example.com/flourish/@@download/flourish_zip',
   };
 
-  // Create a renderer and pass the component wrapped in any necessary context providers (if applicable)
   const component = renderer.create(
     <Provider store={store}>
       <FlourishViewWidget value={mockValue} />
     </Provider>,
   );
 
-  // Create the JSON snapshot of the rendered output
   const json = component.toJSON();
 
-  // Match the snapshot
   expect(json).toMatchSnapshot();
 });
