c26d3e02844932b7eac06698d21280c5ce094000
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2025-02-20 13:18:08 +0200

    Try again

--- src/components/Flourish.jsx
+++ src/components/Flourish.jsx
@@ -1,34 +1,75 @@
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
+// const scripts_map = {};
+//
+const debugStyle = { height: '1px', backgroundColor: 'transparent' };
 
-export default function Flourish({ baseUrl }) {
+function cleanup() {
+  delete window.Flourish;
+  window.FlourishConfig = {
+    public_url: window.location.origin,
+  };
+  delete window._Flourish_visualisation_id;
+  delete window._Flourish_template_id;
+  delete window._flourish_poll_items;
+}
+
+export default function Flourish({ baseUrl, id }) {
   const flourishUrl = `${baseUrl}/@@flourish/index.html`;
+  const nodeRef = useRef(null);
   const scriptUrl = `${baseUrl}/@@flourish/flourish.embed.js`;
 
   useEffect(() => {
     if (!baseUrl) return;
 
-    if (!window.FlourishLoaded) {
-      window.Flourish = { disable_autoload: true };
+    setTimeout(() => {
+      const container = nodeRef.current;
+      if (!container) {
+        // cleanup();
+        return;
+      }
 
-      const script = document.createElement('script');
-      script.src = scriptUrl;
-      script.async = true;
-      script.onload = () => {};
+      container.setAttribute('data-src', flourishUrl);
 
-      document.body.appendChild(script);
-    } else {
-      setTimeout(() => {
-        const domNodes = document.querySelectorAll('.flourish-embed');
-        domNodes.forEach((domNode) => {
-          window.Flourish.loadEmbed(domNode);
-          // eslint-disable-next-line no-console
-          console.log('Loaded Flourish for', domNode);
+      //eslint-disable-next-line no-console
+      console.log(
+        'scripts',
+        document.querySelectorAll('script.flourish-embed-script'),
+      );
+
+      if (
+        document.querySelectorAll('script.flourish-embed-script').length === 0
+      ) {
+        const script = document.createElement('script');
+        script.src = scriptUrl;
+        script.className = 'flourish-embed-script';
+        script.id = id;
+        script.onload = () => {
+          //eslint-disable-next-line no-console
+          console.log('loaded script', id);
+        };
+        document.body.appendChild(script);
+      } else {
+        //eslint-disable-next-line no-console
+        console.log('nodes', document.querySelectorAll('.flourish-embed'));
+        document.querySelectorAll('.flourish-embed').forEach((node) => {
+          //eslint-disable-next-line no-console
+          console.log('Loading embed', node.id);
+          window.Flourish.loadEmbed(node);
         });
-      }, 500);
-    }
+      }
+    }, 1000);
 
-    return () => {};
-  }, [baseUrl, scriptUrl]);
+    return () => {
+      // cleanup();
+    };
+  }, [baseUrl, scriptUrl, id, flourishUrl]);
 
-  return <div className="flourish-embed" data-src={flourishUrl}></div>;
+  return (
+    <div
+      className="flourish-embed"
+      id={id}
+      ref={nodeRef}
+      style={debugStyle}
+    ></div>
+  );
 }
